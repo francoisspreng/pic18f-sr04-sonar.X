4bd0416643e3d16caae6136e5babd3f364802a3d
François <Franois@10.192.93.191>
2016-06-23 21:06:41 +0200

    ca va le faire

--- sonar.c
+++ sonar.c
@@ -21,8 +21,9 @@ typedef enum {
 EtatSonar etatSonar = PAUSE;
 int SonarActif = 0;
 int Compteur = 0;
+int Distance;
 
-void SonarDemmarre() {
+void sonarDemmarre() {
     if (etatSonar == PAUSE){
     etatSonar = TRIGGER;
     }
@@ -31,77 +32,87 @@ void SonarDemmarre() {
 
 void SonarTicTac() {
     if (etatSonar == TRIGGER){
-        etablitTrigger(char SonarActif, char 1);
+        etablitTrigger( SonarActif, 1);
         etatSonar = ATTENTE;
     }
     
-     if (etatSonar == ATTENTE){
-           etablitTrigger(char SonarActif, char 0);
-           if (etablitEntree(char SonarActif)) {
-               etatSonar = COMPTEUR;
-           }
+    if (etatSonar == ATTENTE){
+        etablitTrigger(SonarActif, 0);
+        if (etablitEntree(SonarActif)) {
+            etatSonar = COMPTEUR;
         }
+    }
     
      
     
-     if (etatSonar == COMPTEUR ){
-         if (etablitEntree(char SonarActif) == 1 ){
-             Compteur++;
-         }
+    if (etatSonar == COMPTEUR){
+        if (etablitEntree(SonarActif) == 1 ){
+            Compteur++;
+        }
+        
          
-         if (etablitEntree(char SonarActif) == 0 ){
-             i2cExposeValeur(SonarActif, (Compteur * 29)/58);
-             if (SonarActif < 3){
+         
+        if (etablitEntree(SonarActif) == 0 ){
+            Distance = (Compteur * 29)/58;
+         
+            if (Distance > 255) {
+                Distance = 255;
+            }
+            
+            i2cExposeValeur(SonarActif, Distance);
+            
+            if (SonarActif < 3){
                 SonarActif++;
-             }
-             if (SonarActif >= 3){
+            }
+            
+            if (SonarActif >= 3){
                 SonarActif = 0;
-             }
-             etatSonar = PAUSE;
-         }
-         
-        
-        
-    }
-    void etablitTrigger(char SonarActif, char valeurTrigger) {
-        switch(SonarActif) {
-            case 0:
-                PORTBbits.RB0 = valeurTrigger;
-                break;
-                
-            case 1:
-                PORTBbits.RB1 = valeurTrigger;
-                break;
-                
-            case 2:
-                PORTBbits.RB2 = valeurTrigger;
-                break;
-                
-            case 3:
-                PORTBbits.RB3 = valeurTrigger;
-                break;
+            }
             
+            etatSonar = PAUSE;
         }
     }
+}    
     
-     char etablitEntree(char SonarActif) {
-        switch(SonarActif) {
-            case 0:
-                return PORTAbits.RA0;
-                break;
+    
+    
+void etablitTrigger(char SonarActif, char valeurTrigger) {
+    switch(SonarActif) {
+        case 0:
+            PORTBbits.RB0 = valeurTrigger;
+            break;
                 
-            case 1:
-                return PORTAbits.RA1;
-                break;
+        case 1:
+            PORTBbits.RB1 = valeurTrigger;
+            break;
                 
-            case 2:
-                return PORTAbits.RA2;
-                break;
+        case 2:
+            PORTBbits.RB2 = valeurTrigger;
+            break;
                 
-            case 3:
-                return PORTAbits.RA3;
-                break;
+        case 3:
+            PORTBbits.RB3 = valeurTrigger;
+            break;
             
-        }
     }
 }
+    
+char etablitEntree(char SonarActif) {
+    switch(SonarActif) {
+        case 0:
+            return PORTAbits.RA0;
+                
+        case 1:
+            return PORTAbits.RA1;
+                
+        case 2:
+            return PORTAbits.RA2;
+                
+        case 3:
+            return PORTAbits.RA3;
+            }
+    }
+        
+            
+    
+            
